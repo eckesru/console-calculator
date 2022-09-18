1381170c9bbf3cb6013bd1ea92a7bf399fc3982c
Rudi Eckes <rudi.eckes@gmail.com>
2022-09-18 22:39:01 +0200

    Projektdateien hinzufügen.

---
+++ Console_Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Console_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+
+        }
+    }
+}
