d2af54c47edf9848a6179e0fa0194cba23836509
Rudi Eckes <rudi.eckes@gmail.com>
2022-09-25 21:10:13 +0200

    Kommentare hinzugefügt

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -7,11 +7,17 @@ namespace Console_Calculator
         static void Main(string[] args)
         {
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+
+            //Einlesen und Konvertieren des ersten Werts
             Console.Write("Bitte gib den ersten Summanden ein: ");
             int summand1 = Convert.ToInt32(Console.ReadLine());
+            
+            //Einlesen und Konvertieren des zweiten Werts
             Console.Write("Bitte gib den zweiten Summanden ein: ");
             int summand2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ergebnis: {0}", (summand1 + summand2));
+
+            //Ausgeben der Summe beider Werte
+            Console.WriteLine("Die Summe ist: {0}", (summand1 + summand2));
             Console.ReadLine();
            }
     }
