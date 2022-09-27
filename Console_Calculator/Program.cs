4c78275ca5d3d576f2063c0cca14e277e9d0bde9
Rudi Eckes <rudi.eckes@gmail.com>
2022-09-27 20:09:55 +0200

    Methode Subtrahieren implementiert

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -10,17 +10,17 @@ namespace Console_Calculator
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe zu berechnen
 
             // Einlesen des ersten Werts
-            decimal summand1 = WertEinlesen("Bitte gib den ersten Wert ein: ");
+            decimal wert1 = WertEinlesen("Bitte gib den ersten Wert ein: ");
             
             // Einlesen des zweiten Werts
-            decimal summand2 = WertEinlesen("Bitte gib den zweiten Wert ein: ");
+            decimal wert2 = WertEinlesen("Bitte gib den zweiten Wert ein: ");
 
             // Ergebnis berechnen
-            decimal ergebnis = Addieren(summand1, summand2);
+            decimal ergebnis = Addieren(wert1, wert2);
 
             // Ausgeben der Summe beider Werte
-            Console.WriteLine("Die Summe ist: {0}", ergebnis);
-            WarteAufBenutzerEingabe();
+            Console.WriteLine("Ergebnis: {0}", ergebnis);
+            WarteAufEingabe();
            }
 
         static decimal WertEinlesen(string ausgabeText)
@@ -28,16 +28,16 @@ namespace Console_Calculator
             // Textausgabe des übergebenen Strings auf der Konsole
             Console.Write(ausgabeText);
 
-            // Einlesen der Benutzereingabe auf der Konsole und Konvertieren des Werts
+            // Einlesen der Benutzereingabe auf der Konsole und Konvertieren des Strings in eine Gleitkommazahl
             decimal eingabe = Convert.ToDecimal(Console.ReadLine());
 
             return eingabe;
         }
 
-        static void WarteAufBenutzerEingabe()
+        static void WarteAufEingabe()
         {
             // Textausgabe mit Anweisung zum Beenden der Konsole
-            Console.Write("Zum Beenden die Enter Taste drücken...");
+            Console.Write("Zum Beenden Enter drücken...");
             Console.ReadLine();
         }
 
@@ -45,6 +45,15 @@ namespace Console_Calculator
         {
             // Addition beider übergebenen Werte
             decimal summe = summand1 + summand2;
+
+            return summe;
+
+        }
+        
+        static decimal Subtrahieren(decimal minuend, decimal subtrahend)
+        {
+            // Subtraktion beider übergebenen Werte
+            decimal summe = minuend - subtrahend;
 
             return summe;
         }
