f003c05864b81efba34b6e164f6e121f4147c994
Rudi Eckes <rudi.eckes@gmail.com>
2022-09-27 18:34:38 +0200

    Methode WertEinlesen implementiert

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace Console_Calculator
 {
@@ -10,27 +11,37 @@ namespace Console_Calculator
 
             //Einlesen und Konvertieren des ersten Werts
             Console.Write("Bitte gib den ersten Summanden ein: ");
-            decimal summand1 = Convert.ToDecimal(Console.ReadLine());
+            decimal summand1 = WertEinlesen();
             
             //Einlesen und Konvertieren des zweiten Werts
             Console.Write("Bitte gib den zweiten Summanden ein: ");
-            decimal summand2 = Convert.ToDecimal(Console.ReadLine());
+            decimal summand2 = WertEinlesen();
+
+            //Ergebnis berechnen
+            decimal summe = Addieren(summand1, summand2);
 
             //Ausgeben der Summe beider Werte
-            decimal summe = summand1 + summand2;
             Console.WriteLine("Die Summe ist: {0}", summe);
             WarteAufBenutzerEingabe();
            }
 
+        static decimal WertEinlesen()
+        {
+            decimal eingabe = Convert.ToDecimal(Console.ReadLine());
+
+            return eingabe;
+        }
+
         static void WarteAufBenutzerEingabe()
         {
-            Console.Write("Zum Beenden die Taste Enter drücken...");
+            Console.Write("Zum Beenden die Enter Taste drücken...");
             Console.ReadLine();
         }
 
         static decimal Addieren(decimal summand1, decimal summand2)
         {
             decimal summe = summand1 + summand2;
+
             return summe;
         }
     }
