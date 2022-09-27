60583e3ec1d99bbcad5bcbe03fa9a7b1306417bd
Rudi Eckes <rudi.eckes@gmail.com>
2022-09-27 21:01:45 +0200

    Auslagern der Konvertierung aus der Methode WertEinlesen

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -10,35 +10,37 @@ namespace Console_Calculator
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe zu berechnen
 
             // Einlesen des ersten Werts
-            decimal wert1 = WertEinlesen("Bitte gib den ersten Wert ein: ");
+            string wert1_string = WertEinlesen("Bitte gib den ersten Wert ein: ");
             
             // Einlesen des zweiten Werts
-            decimal wert2 = WertEinlesen("Bitte gib den zweiten Wert ein: ");
+            string wert2_string = WertEinlesen("Bitte gib den zweiten Wert ein: ");
+
+            // Einlesen des Rechenoperators
+            string rechenoperator = WertEinlesen("Bitte gibt den Rechenoperator ein: ");
+
+            // Konvertierung der Eingabewerte in Gleitkommazahlen
+            // TODO: Auslagern in Methode
+            decimal wert1 = Convert.ToDecimal(wert1_string);
+            decimal wert2 = Convert.ToDecimal(wert2_string);
 
             // Ergebnis berechnen
             decimal ergebnis = Addieren(wert1, wert2);
 
             // Ausgeben der Summe beider Werte
-            Console.WriteLine("Ergebnis: {0}", ergebnis);
-            WarteAufEingabe();
+            Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
+            WertEinlesen("Zum Beenden Enter drücken...");
            }
 
-        static decimal WertEinlesen(string ausgabeText)
+
+        static string WertEinlesen(string ausgabeText)
         {
             // Textausgabe des übergebenen Strings auf der Konsole
             Console.Write(ausgabeText);
 
-            // Einlesen der Benutzereingabe auf der Konsole und Konvertieren des Strings in eine Gleitkommazahl
-            decimal eingabe = Convert.ToDecimal(Console.ReadLine());
+            // Einlesen der Eingabe auf der Konsole
+            string eingabe = Console.ReadLine()!;
 
             return eingabe;
-        }
-
-        static void WarteAufEingabe()
-        {
-            // Textausgabe mit Anweisung zum Beenden der Konsole
-            Console.Write("Zum Beenden Enter drücken...");
-            Console.ReadLine();
         }
 
         static decimal Addieren(decimal summand1, decimal summand2)
