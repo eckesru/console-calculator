ca4e1b3541182f4dccdda762bc54c269303f69e8
Rudi Eckes <rudi.eckes@gmail.com>
2022-10-03 13:36:20 +0200

    Methoden WertEinlesen und ErgebnisAusgabe in Klasse ConsoleView verschoben

---
+++ Console_Calculator/ConsoleView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator
+{
+    internal class ConsoleView
+    {
+        public ConsoleView() { }
+
+        public void ErgebnisAusgabe(decimal wert1, string rechenoperator, decimal wert2, decimal ergebnis)
+        {
+            switch (rechenoperator)
+            {
+
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "%":
+                    // String-Ausgabe der Berechnung und des Ergebnisses
+                    Console.WriteLine("Ergebnis: " + wert1 + " " + rechenoperator + " " + wert2 + " = {0}", ergebnis);
+                    break;
+
+                default:
+                    Console.WriteLine("Ungültige Eingabe des Rechenoperators.");
+                    break;
+            }
+        }
+
+        public string WertEinlesen(string ausgabeText)
+        {
+            // Textausgabe des übergebenen Strings auf der Konsole
+            Console.Write(ausgabeText);
+
+            // Einlesen der Eingabe auf der Konsole
+            string eingabe = Console.ReadLine()!;
+
+            return eingabe;
+        }
+
+    }
+}
--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -7,16 +7,15 @@ namespace Console_Calculator
     {
         static void Main(string[] args)
         {
-            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe zu berechnen
-
             // Einlesen des ersten Werts
-            string wert1_string = WertEinlesen("Bitte gib den ersten Wert ein: ");
+            ConsoleView consoleView = new ConsoleView();
+            string wert1_string = consoleView.WertEinlesen("Bitte gib den ersten Wert ein: ");
 
             // Einlesen des Rechenoperators
-            string rechenoperator = WertEinlesen("Bitte gib den Rechenoperator ein (+, -, *, /, oder %): ");
+            string rechenoperator = consoleView.WertEinlesen("Bitte gib den Rechenoperator ein (+, -, *, /, oder %): ");
 
             // Einlesen des zweiten Werts
-            string wert2_string = WertEinlesen("Bitte gib den zweiten Wert ein: ");
+            string wert2_string = consoleView.WertEinlesen("Bitte gib den zweiten Wert ein: ");
 
             // Konvertierung der Eingabewerte in Gleitkommazahlen
             // TODO: Auslagern in Methode
@@ -28,41 +27,10 @@ namespace Console_Calculator
             calculatorModel.Berechne(wert1, rechenoperator, wert2);
 
             // Ergebnis ausgeben
-            ErgebnisAusgabe(wert1, rechenoperator, wert2, calculatorModel.Ergebnis);
+            consoleView.ErgebnisAusgabe(wert1, rechenoperator, wert2, calculatorModel.Ergebnis);
 
             // Ende des Programms
-            WertEinlesen("Zum Beenden Enter drücken...");
-        }
-
-        static void ErgebnisAusgabe(decimal wert1, string rechenoperator, decimal wert2, decimal ergebnis)
-        {
-            switch (rechenoperator)
-            {
-
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                case "%":
-                    // String-Ausgabe der Berechnung und des Ergebnisses
-                    Console.WriteLine("Ergebnis: " + wert1 + " " + rechenoperator + " " + wert2 + " = {0}", ergebnis);
-                    break;
-
-                default:
-                    Console.WriteLine("Ungültige Eingabe des Rechenoperators.");
-                    break;
-            }
-        }
-
-        static string WertEinlesen(string ausgabeText)
-        {
-            // Textausgabe des übergebenen Strings auf der Konsole
-            Console.Write(ausgabeText);
-
-            // Einlesen der Eingabe auf der Konsole
-            string eingabe = Console.ReadLine()!;
-
-            return eingabe;
+            consoleView.WertEinlesen("Zum Beenden Enter drücken...");
         }
 
 
